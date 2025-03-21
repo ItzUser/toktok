7acc21773d43ca208acb24b266aefa49741f6b91
ItzUser <129721272+ItzUser@users.noreply.github.com>
2025-03-21 23:09:24 +0700

    Update client.js

--- api/client.js
+++ api/client.js
@@ -1,29 +1,187 @@
-const SnapTikClient = require('./client.js'); // Path ke client.js di folder API
 const axios = require('axios');
+const cheerio = require('cheerio');
+const FormData = require('form-data');
 
-module.exports = async (req, res) => {
-    const { url } = req.query;
-    if (!url) return res.status(400).json({ error: 'URL TikTok wajib diisi!' });
+class Resource {
+  constructor(url, index) {
+    this.index = index;
+    this.url = url;
+  }
 
-    const snapTik = new SnapTikClient();
+  download(config = {}) {
+    return axios({
+      url: this.url,
+      type: 'stream',
+      ...config
+    });
+  }
+}
 
-    try {
-        const result = await snapTik.process(url);
+class SnapTikClient {
+  constructor(config = {}) {
+    this.axios = axios.create(this.config = {
+      baseURL: 'https://dev.snaptik.app',
+      ...config,
+    });
+  }
 
-        if (result.type === 'video' || result.type === 'photo' || result.type === 'slideshow') {
-            const videoUrl = result.data.sources[0].url;
+  async get_token() {
+    const {
+      data
+    } = await this.axios({
+      url: '/'
+    });
+    const $ = cheerio.load(data);
+    return $('input[name="token"]').val();
+  }
 
-            const response = await axios.get(videoUrl, { responseType: 'stream' });
+  async get_script(url) {
+    const form = new FormData();
+    const token = await this.get_token();
 
-            res.setHeader('Content-Disposition', 'attachment; filename="tiktok_video.mp4"');
-            res.setHeader('Content-Type', 'video/mp4');
+    form.append('token', token);
+    form.append('url', url);
 
-            response.data.pipe(res);
-        } else {
-            res.status(500).json({ error: 'Gagal mendapatkan video/foto!' });
+    const {
+      data
+    } = await this.axios({
+      url: '/abc2.php',
+      method: 'POST',
+      data: form
+    });
+
+    return data;
+  }
+
+  async eval_script(script1) {
+    const script2 = await new Promise(resolve => Function('eval', script1)(resolve));
+    return new Promise((resolve, reject) => {
+      let html = '';
+      const [
+        k,
+        v
+      ] = [
+        'keys',
+        'values'
+      ].map(x => Object[x]({
+        $: () => Object.defineProperty({
+          remove() {},
+          style: {
+            display: ''
+          }
+        }, 'innerHTML', {
+          set: t => (html = t)
+        }),
+        app: {
+          showAlert: reject
+        },
+        document: {
+          getElementById: () => ({
+            src: ''
+          })
+        },
+        fetch: a => {
+          return resolve({
+            html,
+            oembed_url: a
+          }), {
+            json: () => ({
+              thumbnail_url: ''
+            })
+          };
+        },
+        gtag: () => 0,
+        Math: {
+          round: () => 0
+        },
+        XMLHttpRequest: function() {
+          return {
+            open() {},
+            send() {}
+          }
+        },
+        window: {
+          location: {
+            hostname: 'snaptik.app'
+          }
         }
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).json({ error: 'Terjadi kesalahan saat memproses link!' });
-    }
-};
+      }));
+
+      Function(...k, script2)(...v);
+    });
+  }
+
+  async get_hd_video(token) {
+    const {
+      data: {
+        error,
+        url
+      }
+    } = await this.axios({
+      url: '/getHdLink.php?token=' + token
+    });
+
+    if (error) throw new Error(error);
+    return url;
+  }
+
+  async parse_html(html) {
+    const $ = cheerio.load(html);
+    const is_video = !$('div.render-wrapper').length;
+
+    return is_video ? await (async () => {
+      const hd_token = $('div.video-links > button[data-tokenhd]').data('tokenhd');
+      const hd_url = new URL(await this.get_hd_video(hd_token));
+      const token = hd_url.searchParams.get('token');
+      const {
+        url
+      } = JSON.parse(atob(token.split('.')[1].replace(/-/g, '+').replace(/_/g, '/')));
+
+      return {
+        type: 'video',
+        data: {
+          sources: [
+            url,
+            hd_url.href,
+            ...$('div.video-links > a:not(a[href="/"])').toArray()
+            .map(elem => $(elem).attr('href'))
+            .map(x => x.startsWith('/') ? this.config.baseURL + x : x)
+          ].map((...x) => new Resource(...x))
+        }
+      };
+    })() : (x => x.data.photos.length == 1 ? ({
+      ...x,
+      type: 'photo',
+      data: {
+        sources: x.data.photos[0].sources
+      }
+    }) : x)({
+      type: 'slideshow',
+      data: {
+        photos: $('div.columns > div.column > div.photo').toArray().map(elem => ({
+          sources: [
+            $(elem).find('img[alt="Photo"]').attr('src'),
+            $(elem).find('a[data-event="download_albumPhoto_photo"]').attr('href')
+          ].map((...x) => new Resource(...x))
+        }))
+      }
+    });
+  }
+
+  async process(url) {
+    const script = await this.get_script(url);
+    const {
+      html,
+      oembed_url
+    } = await this.eval_script(script);
+
+    const res = {
+      ...(await this.parse_html(html)),
+      url
+    };
+
+    return res.data.oembed_url = oembed_url, res;
+  }
+}
+
+module.exports = SnapTikClient;
